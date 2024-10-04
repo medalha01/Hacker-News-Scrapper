a3113da7261dc7d4a7689220b657cf7f70233a26
medalha01 <hiperboss2009@gmail.com>
2024-10-04 09:54:00 -0300

    New Header

--- hacker_news.script.py
+++ hacker_news.script.py
@@ -67,7 +67,13 @@ async def scrape_hacker_news(url):
 
     async with aiohttp.ClientSession(
         headers={
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0"
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "TE": "Trailers"
         }
     ) as session:
         response = await fetch_url(session, url)
